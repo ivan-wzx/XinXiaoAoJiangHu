75e23641bba78238254537dd53d37062b2b54185
Ivan Wu <ivan.wzx@gmail.com>
2020-08-26 03:02:23 -0700

    refactor

--- script.py
+++ script.py
@@ -2,7 +2,6 @@ from PIL import ImageGrab
 import pyautogui
 import pydirectinput
 import win32api, win32con
-#import cv2
 import numpy
 import time
 
@@ -17,19 +16,17 @@ def click(x,y):
 
 
 confirm_loc=[0.761, 0.735]
-confirm_loc2=[0.633, 0.570]
-#thres=0.8
-#t_confirm=cv2.imread(".\\templates\\yiriyou_jixu.png")
-#t_confirm=cv2.cvtColor(t_confirm, cv2.COLOR_BGR2GRAY)
+#confirm_loc2=[0.633, 0.570]
 
 if __name__ == "__main__":
-    print("一日游自动代刷脚本设置：模拟器开全屏，使用前请先组好队，队伍自动跟随，开好自动攻击，领好一日游任务。本脚本会在每轮一日游完成后自动点确认和继续。")
+    print("一日游自动代刷脚本设置：模拟器开全屏，使用前请先组好队，设置队伍自动跟随，设置自动攻击，先领好一日游任务。")
+    print("启动本脚本后请回到游戏全屏界面，本脚本会在每轮一日游完成后自动点确认和继续。")
     print("本纯绿色脚本由玩家*肝die冲鸭*为您开发")
     screenWidth, screenHeight = pydirectinput.size()
     print("Screen resolution: {}x{}".format(screenWidth, screenHeight))
-    input("回车确认，确认后该程序会自动最小化")
-    cur_win = pyautogui.getActiveWindow()
-    cur_win.minimize()
+    #input("回车确认，确认后该程序会自动最小化")
+    #cur_win = pyautogui.getActiveWindow()
+    #cur_win.minimize()
     while True:
         time.sleep(5)
         pil_image=ImageGrab.grab()
@@ -39,15 +36,10 @@ if __name__ == "__main__":
         right_stripe_intensity = numpy.percentile(gray[:, int(screenWidth*0.925):screenWidth-10],99)
         if left_stripe_intensity > 100 or right_stripe_intensity > 100:
             continue
-        
         loc=[int(screenWidth*confirm_loc[1]), int(screenHeight*confirm_loc[0])]
-        #click(*loc)
-        loc2=[int(screenWidth*confirm_loc2[1]), int(screenHeight*confirm_loc2[0])]
+        #loc2=[int(screenWidth*confirm_loc2[1]), int(screenHeight*confirm_loc2[0])]
         try:
-            #pydirectinput.moveTo(1, 1)
             pydirectinput.click(*loc)
-            #pydirectinput.click()
-            #pydirectinput.doubleClick()
         except Exception as ex:
             print(str(ex))
 
